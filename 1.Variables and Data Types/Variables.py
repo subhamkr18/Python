af6c35e98d0f6ed54749d652b2bf6d84f9616f2d
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-09-24 00:18:09 +0530

    Variables in python and rules for Variable naming

---
+++ 1.Variables and Data Types/Variables.py
@@ -0,0 +1,24 @@
+name="Subham"
+age=20
+cgpa=8.2
+gender="Male"
+
+""" in python their is no need to specify data type
+ like int, float string or something else it 
+ automatically determine while compileing the code"""
+
+
+print(name)
+print(age)
+print(cgpa)
+print(gender)
+
+"""
+Rules for variable naming
+
+1. must stert with a letter or underscore(_)
+2. can contain letters, digits, andunderscore
+3.cannot start with a digit 
+4. cannot use Python Heywords (if, for,while.....)
+5. Names are case-Sensitive -> age != Age !=AGE
+"""
