f7d6d3936e7c9c8e284ea9338ca0b666f4ef5041
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-09-24 00:37:05 +0530

    Type conversion in python

---
+++ 1.Variables and Data Types/Type_COnversion.py
@@ -0,0 +1,12 @@
+num1="100"
+num2="200"
+# explicit conversion
+num3=int(num1)
+num4=int(num2)
+
+print(num3 +num4)
+
+print(int(num1) + int(num2))
+
+a=500.5
+print(int(a)) # always gives floor value
