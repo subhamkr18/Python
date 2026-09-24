774a499304a55be8f25f81089583a8e68ba6c775
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-09-24 23:42:10 +0530

    Logical operators in python 'AND,OR,NOT

---
+++ 3.Arithmetic Operators/Logical_operators.py
@@ -0,0 +1,17 @@
+# AND, OR, NOT
+
+chemistry = 45
+physics = 34 
+
+# logical operators also gives output in True or False
+
+# AND - All given condition should be true then it will give true
+
+print(chemistry>33 and physics > 33)
+
+# OR- gives True if any given condition is true Flase if all given contion is false
+
+print(chemistry>33 or physics>33)
+
+# Not- It reverse the result 
+print(not chemistry > 33 and not physics>33)
