28fa53df8067fc80628858a878c3d0d97d5fcc99
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-09-24 23:36:10 +0530

    Comparision operators in python

---
+++ 3.Arithmetic Operators/Comparison_operators.py
@@ -0,0 +1,12 @@
+a = 10
+b = 5
+
+# comparision operators always give boolean output True or False
+
+print("a Equal to b : ",a==b)
+print(" a Grater than equal to b: ",a>=b)
+print(" a Less than equal to b: ",a<=b)
+print(" a Greater than b: ",a>b)
+print(" a Less than b: ",a<b)
+print(" a Not equal to b: ",a!=b)
+
