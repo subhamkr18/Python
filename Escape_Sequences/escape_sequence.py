ed754fb7631d3a45863aeabfb9f153a74b862d08
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-09-24 23:19:19 +0530

    use of Escape sequences in python

---
+++ Escape_Sequences/escape_sequence.py
@@ -0,0 +1,15 @@
+'''
+\n- new line
+\t- Tab space
+\\- Backslash
+\"- Double quotes
+\'- Single quote
+
+'''
+
+print("Hello\nSubham !")
+print("Hello\tSubham!") 
+print("Hi\\\\\\\nSubham")
+
+# My name is "Subham" Kumar
+print("My name is \"Subham\" Kumar")
