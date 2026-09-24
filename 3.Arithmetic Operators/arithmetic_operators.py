8d476a1de28c9ffe0d9198cbd847ece1b0204f98
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-09-24 23:28:34 +0530

    Arithmetic operators in python

---
+++ 3.Arithmetic Operators/arithmetic_operators.py
@@ -0,0 +1,19 @@
+a = 5
+b = 10
+
+print("Addition: ",a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
+
+# Floor division gives integer
+print("Floor Division: ",b//a)
+
+#Exponent 
+
+print("2 power 6 is: ",2**6)
+
+print("100 power 0.5: ",100**0.5)
+
+# modulus (remainder)
+print("Remainder: ",a%b)
