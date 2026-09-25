4bee6302503f30c729dcd4b3e215ff68b7bc7443
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-09-26 00:03:32 +0530

    Use of Nested if else statement

---
+++ 5.Conditional Statements/Nested_if_else.py
@@ -0,0 +1,14 @@
+# Age >= 18
+# Certificate -> True
+
+age = 45
+Certificate = True
+
+if age >=18:
+    if Certificate == True:
+        print("You will we hired")
+    else:
+        print("Sorry can't hired we don't have Certificate")
+
+else:
+    print("You are Underage you are not eligible")
