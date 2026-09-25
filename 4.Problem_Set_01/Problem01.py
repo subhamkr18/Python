f38a7a5213e3c2d3dd98b82880f53d303f27aa1e
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-09-25 22:49:33 +0530

    Problem on Arithmetic Operation

---
+++ 4.Problem_Set_01/Problem01.py
@@ -0,0 +1,17 @@
+"""
+Take two numbers as input from the user. Print their sum, difference,
+product, and remainder.
+
+"""
+
+num1 = int(input("Enter 1st Number: "))
+num2 = int(input("Enter 2st Number: "))
+
+# calculating and printing sum 
+print(f"Sum of {num1} and {num2} : {num1+num2}")
+# Calculating and printing Difference 
+print(f"Difference of {num1} nad {num2}: {num1 - num2}")
+# Calculating and Printing Product
+print(f"Product of{num1} and {num2}: {num1 * num2}")
+# Calculating and Printing Remainder
+print(f"Remainder of {num1} and {num2}: {num1 % num2}")
